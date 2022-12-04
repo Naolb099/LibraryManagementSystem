9a2fbd32318d70b44ef0097aef573ac35849ae66
Gurmessa <naoljemal99@gmail.com>
2022-12-04 08:50:26 -0600

    book issue page created

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Publisher()
+        {
+            return View();
+        }
+
+        public IActionResult AuthorIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/userProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class userProfileController : Controller
+    {
+        public IActionResult upIndex()
+        {
+            return View();
+        }
+    }
+}
